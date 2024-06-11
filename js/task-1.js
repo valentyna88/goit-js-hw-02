d98c5ec4cb2fba193e6ec4c72a89d727520ceb27
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-06-11 10:08:24 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   let totalPrice = quantity * pricePerDroid;
   if (totalPrice > customerCredits) {
-    return "Insufficient funds!"
+    return "Insufficient funds!";
   } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
